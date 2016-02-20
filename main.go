d014118d3ebea123392177cf8ce28d4df796de0d
Brian Foshee <brianfoshee@gmail.com>
2016-02-20 16:54:37 -0500

    working policy and signature

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	htemplate "html/template"
 	"net/http"
+	"os"
 	"text/template"
 	"time"
 )
@@ -32,8 +33,13 @@ func main() {
 	}
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
+		// Create a policy using UTF-8 encoding.
+		// Convert the UTF-8-encoded policy to Base64. The result is the string to sign.
+		// Create the signature as an HMAC-SHA256 hash of the string to sign. You will provide the signing key as key to the hash function.
+		// Encode the signature by using hex encoding.
+
 		now := time.Now().UTC()
-		key := ""
+		key := os.Getenv("AWS_ACCESS_KEY_ID")
 		p := Payload{
 			Expiration:        now.Add(24 * time.Hour).Format(time.RFC3339),
 			Bucket:            "brianfoshee",
@@ -63,30 +69,32 @@ func main() {
 		policy := make([]byte, b64.EncodedLen(buf.Len()))
 		b64.Encode(policy, buf.Bytes())
 
-		sec := ""
+		sec := os.Getenv("AWS_SECRET_ACCESS_KEY")
 
-		// Create a policy using UTF-8 encoding.
-		// Convert the UTF-8-encoded policy to Base64. The result is the string to sign.
-		// Create the signature as an HMAC-SHA256 hash of the string to sign. You will provide the signing key as key to the hash function.
-		// Encode the signature by using hex encoding.
+		s256 := sha256.New
+		dk := hmac.New(s256, []byte("AWS4"+sec))
+		dk.Write([]byte(now.Format("20060102")))
 
-		// create signing key
-		signKey := hmac.New(sha256.New, []byte("AWS4"+sec))
-		signKey.Write([]byte(now.Format("20060102")))
-		signKey.Write([]byte("us-east-1"))
-		signKey.Write([]byte("s3"))
-		signKey.Write([]byte("aws4_request"))
-		// hash the signing key with the base64'd policy - this is the signature
-		signKey.Write(policy)
+		drk := hmac.New(s256, dk.Sum(nil))
+		drk.Write([]byte("us-east-1"))
+
+		drsk := hmac.New(s256, drk.Sum(nil))
+		drsk.Write([]byte("s3"))
+
+		signingKey := hmac.New(s256, drsk.Sum(nil))
+		signingKey.Write([]byte("aws4_request"))
+
+		signature := hmac.New(s256, signingKey.Sum(nil))
+		signature.Write([]byte(policy))
 
 		// encode signature using hex encoding
-		signature := make([]byte, hex.EncodedLen(signKey.Size()))
-		hex.Encode(signature, signKey.Sum(nil))
+		encsig := make([]byte, hex.EncodedLen(signature.Size()))
+		hex.Encode(encsig, signature.Sum(nil))
 
 		// fill out the form
 		f := Form{
 			Policy:    string(policy),
-			Signature: string(signature),
+			Signature: string(encsig),
 			Payload:   p,
 		}
 
@@ -145,75 +153,3 @@ var form = `<!doctype html>
   </form>
 </body>
 </html>`
-
-/*
-{ "expiration": "2015-12-30T12:00:00.000Z",
-  "conditions": [
-    {"bucket": "sigv4examplebucket"},
-    ["starts-with", "$key", "user/user1/"],
-    {"acl": "public-read"},
-    {"success_action_redirect": "http://sigv4examplebucket.s3.amazonaws.com/successful_upload.html"},
-    ["starts-with", "$Content-Type", "image/"],
-
-    {"x-amz-credential": "AKIAIOSFODNN7EXAMPLE/20151229/us-east-1/s3/aws4_request"},
-    {"x-amz-algorithm": "AWS4-HMAC-SHA256"},
-    {"x-amz-date": "20151229T000000Z" }
-  ]
-}
-*/
-
-/*
-
-  def request_equirect_upload
-    s3_options = Pano.default_s3_options
-    policy = @pano.create_s3_policy(ENV["S3_BUCKET"], s3_options)
-    signature = @pano.create_s3_signature(ENV["S3_SECRET"], policy)
-    data = {
-      :post_to        => "https://s3.amazonaws.com/#{ENV["S3_BUCKET"]}/",
-      :signature      => signature,
-      :acl            => s3_options[:acl],
-      :policy         => policy,
-      :AWSAccessKeyId => ENV["S3_KEY"],
-      :key            => "tours/#{@pano.id}/images/equirect.jpg",
-      :pano           => {:id => @pano.id}
-    }
-  end
-
-  def create_s3_policy(bucket, options)
-    str = "{'expiration': '#{options[:expiration_date]}',
-      'conditions': [
-        {'bucket': '#{bucket}'},
-        {'acl': '#{options[:acl]}'},
-        {'success_action_status': '201'},
-        ['content-length-range', 0, #{options[:max_filesize]}],
-        ['starts-with', '$key', 'tours/#{self.id}/images'],"
-
-    if options[:add_web_params] == true
-      str += "['starts-with', '$Content-Type', '#{options[:content_type]}'],
-      ['starts-with', '$name', ''],
-      ['starts-with', '$Filename', ''],"
-    end
-
-    str += "]}"
-
-    Base64.encode64(str).gsub(/\n|\r/, '')
-  end
-
-  def create_s3_signature(key, policy)
-    Base64.encode64(
-      OpenSSL::HMAC.digest(
-        OpenSSL::Digest::Digest.new('sha1'),
-        key, policy)).gsub("\n","")
-  end
-
-  def self.default_s3_options
-    options = {}
-    options[:acl] ||= 'private'
-    options[:expiration_date] ||= 10.hours.from_now.utc.iso8601
-    options[:max_filesize] ||= 100.megabytes
-    options[:content_type] ||= 'image/' # Videos would be binary/octet-stream
-    options[:filter_title] ||= 'Images'
-    options[:filter_extentions] ||= 'jpg'
-    options
-  end
-*/
